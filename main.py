ea8d979188266a3feb5372c83da8441f0336ad2c
Miskat-UL <53708135+miShowmik@users.noreply.github.com>
2021-07-02 00:50:01 +0600

    file

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def menu():
         -Enter B to Retrive book information.
         -Enter D to delete books.
         -Enter R to mark book as read
+        -Enter E for clear the database
         -press q to quit
         """
         )
@@ -20,6 +21,8 @@ def menu():
             read_books()
         elif user_input == 'd':
             delete_books()
+        # elif user_input == 'e':
+        #     clear_data()
         else:
             print('unknown command, please try again ')
 
@@ -28,6 +31,7 @@ def menu():
                 -Enter B to Retrive book information.
                 -Enter D to delete books.
                 -Enter R to mark book as read
+                -Enter E for clear the database
                 -press q to quit
                 """
                            )
@@ -37,14 +41,14 @@ def add_books():
     name = input('Enter the book name: ')
     author = input('Enter author name: ')
 
-    database.add_books(name,author)
+    database.add_books(name, author)
 
 
 def list_books():
     books = database.show_books()
     for book in books:
-        read = 'YES' if book['read'] else 'NO'
-        print(f"{book['name']} by {book['author']} read:{read}")
+        read = 'YES' if book['read'] == '1' else 'NOO'
+        print(f"{book['name']} by {book['author']}, read:{read}")
 
 
 def read_books():
@@ -56,5 +60,9 @@ def delete_books():
     name = input('Enter the book name you want to remove: ')
 
     database.delete(name)
+
+
+# def clear_data():
+#     database.clear()
 
 menu()
--- utlis/database.py
+++ utlis/database.py
@@ -1,20 +1,52 @@
-book_list = []
+#saving data to CSV files.
 
 
-def add_books(name,author):
-    book_list.append({'name': name, 'author': author, 'read': False})
+book_list = 'data.txt'
+
+
+def create_book_table():
+    with open(book_list, 'w') as file:
+        pass
+
+
+
+create_book_table()
+
+def add_books(name, author):
+    with open(book_list, 'a') as file:
+        file.write(f"{name},{author}, 0\n")
 
 
 def show_books():
-    return book_list
+    with open(book_list, 'r') as file:
+        list_lines = [line.strip().split(',') for line in file.readlines()]  # [[name, author, read],[name, author, read]]
+
+    return [
+        {'name': line[0], 'author': line[1], 'read': line[2]}
+        for line in list_lines
+    ]
 
 
 def mark_books_as_read(name):
-    for book in book_list:
+    books = show_books()
+    for book in books:
         if book['name'] == name:
-            book['read'] = True
+            book['read'] = '1'
+    _save_book_again(books)
+
+
+def _save_book_again(books):
+    with open(book_list, 'w') as file:
+        for book in books:
+            file.write(f"{book['name']},{book['author']},{book['read']}\n")
 
 
 def delete(name):
-    global book_list
-    book_list = [book for book in book_list if book['name'] != name]
+    books = show_books()
+    books = [book for book in books if book['name'] != name]
+    _save_book_again(books)
+
+
+# def clear():
+#     with open(book_list, 'r+') as file:
+#         file.truncate()
